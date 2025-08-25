99b45346d2d9b286eee0626a8126b7197d1f65dc
mauricioroos <73857496+mauricioroos@users.noreply.github.com>
2025-08-25 16:19:36 -0300

    cadastrofuncionado

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name ="url_index"),
     path('produtos', views.produtos, name ="url_produtos"),
-
+    path('cad_produto',views.cad_produto, name="url_cad_produto"),
+  #  path('criar_produto',views.criar, name="url_criar_produto"),#
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Produto
+from django.http.response import HttpResponse
 
 def index(request):
     context = {
@@ -14,4 +15,22 @@ def produtos(request):
         
     }
     return render(request, 'produto.html', context)
+
+def cad_produto(request):
+    if request.method == "GET":
+        return render(request,'cad_produto.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        preco = request.POST.get('preco').replace(',','.')
+        qtde = request.POST.get('qtde')
+    
+        produtos =  Produto(
+            nome = nome,
+            preco = preco,
+            qtde = qtde,
+        )
+        produtos.save()
+        return redirect('url_produtos')
+
+
 
