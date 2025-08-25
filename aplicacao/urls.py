b8325950b6b86da8f6ed93b26edf79c33735738d
mauricioroos <73857496+mauricioroos@users.noreply.github.com>
2025-08-25 17:43:48 -0300

    botao atualizar

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('', views.index, name ="url_index"),
-    path('produtos', views.produtos, name ="url_produtos"),
-    path('cad_produto',views.cad_produto, name="url_cad_produto"),
-  #  path('criar_produto',views.criar, name="url_criar_produto"),#
+    path('', views.index, name="url_index"),
+    path('produtos', views.produtos, name="url_produtos"),
+    path('cad_produto', views.cad_produto, name="url_cad_produto"),
+    path('atualizar_produto/<int:id>', views.atualizar_produto, name="url_atualizar_produto"),
+    path('apagar_produto/<int:id>', views.apagar_produto, name="url_apagar_produto"),
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,36 +1,58 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Produto
 from django.http.response import HttpResponse
 
+# Create your views here.
+
 def index(request):
     context = {
-        "texto": "Olá mundo!",
+        'texto': "Olá mundo!",
     }
-    return render(request,'index.html', context)
+    return render(request, 'index.html', context)
 
 def produtos(request):
     produtos = Produto.objects.all()
-    context ={
-        'produtos': produtos
-        
+    context = {
+        'produtos' : produtos,
     }
     return render(request, 'produto.html', context)
 
 def cad_produto(request):
+
     if request.method == "GET":
-        return render(request,'cad_produto.html')
+        return render(request, 'cad_produto.html')
     elif request.method == "POST":
         nome = request.POST.get('nome')
-        preco = request.POST.get('preco').replace(',','.')
+        preco = request.POST.get('preco').replace(',', '.')
         qtde = request.POST.get('qtde')
-    
-        produtos =  Produto(
+
+        produto = Produto(
             nome = nome,
             preco = preco,
-            qtde = qtde,
+            qtde = qtde
         )
-        produtos.save()
+        produto.save()
         return redirect('url_produtos')
+    
+def atualizar_produto(request, id):
+    #prod = Produto.objects.get(id=id)
+    prod = get_object_or_404(Produto, id=id)
+    if request.method == "GET":
+        context = {
+            'prod': prod,
+        }
+        return render(request, 'atualizar_produto.html', context)
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        preco = request.POST.get('preco').replace(',', '.')
+        qtde = request.POST.get('qtde')
 
+        prod.nome = nome
+        prod.preco = preco
+        prod.qtde = qtde
+        prod.save()
 
+    return redirect('url_produtos')
 
+def apagar_produto(request):
+    pass
