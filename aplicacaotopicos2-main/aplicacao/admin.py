53d90abcc8ecf2e4ae123264a468310b430fab11
mauricioroos <73857496+mauricioroos@users.noreply.github.com>
2025-08-30 08:45:17 -0300

    Delete aplicacaotopicos2-main directory

--- aplicacaotopicos2-main/aplicacao/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Produto
-
-class ProdutoAdm(admin.ModelAdmin):
-    list_display = ('nome', 'preco', 'quantidade')
-
-admin.site.register(Produto, ProdutoAdm)
