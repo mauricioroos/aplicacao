60e8ac9d21793b5774ed319a42af1f4a6936c52e
mauricioroos <mauricio_roos@outlook.com>
2025-09-01 18:17:07 -0300

    sistema de vendas

--- aplicacao/models.py
+++ aplicacao/models.py
@@ -4,31 +4,39 @@ class Produto(models.Model):
     nome = models.CharField("Nome", max_length=200,null = True)
     preco = models.DecimalField("preco", decimal_places=2,max_digits = 8)
     quantidade = models.PositiveIntegerField("Quantidade", default=0,null = True)
-
     def __str__(self):
         return self.nome
     
-class cliente(models.Model):
-    nome = models.CharField("Nome", max_length=200,null = True)
-    email = models.DecimalField("email", decimal_places=2,max_digits = 8)
-    
-class PerfilCliente(models.Model):
-    endereco = models.CharField("endereco", max_length=200,null = True)
-    telefone = models.DecimalField("telefone", decimal_places=2,max_digits = 8)
+class Perfil(models.Model):
+    cliente = models.OneToOneField(cliente,on_delete= models.CASCADE)
+    telefone = models.CharField("telefone", max_legth=20)
+    rua = models.CharField("Rua",max_length=200)
+    numero = models.PositiveBigIntegerField("Nº",max_length=10)
+    cep = models.CharField("CEP",max_length=20)
+    bairro = models.CharField("Bairro",max_length=50)
+    cidade = models.CharField("Cidade",max_length=50)
+    complemento = models.CharField("Complemento",max_length=200)
+   
+    def __str__(self):
+         return f"{self.rua}, {self.numero} - {self.bairro}"
 
-class PerfilCliente(models.Model):
-    endereco = models.CharField("endereco", max_length=200,null = True)
-    telefone = models.DecimalField("telefone", decimal_places=2,max_digits = 8)
-    
 class venda(models.Model):
         data = models.DateTimeField("Data", null= True)
+        venda = models.ManyToManyField(
+              Produto,
+              through='vendaProduto',)
+
+    def __str_(self):
+         return str(self.cliente)
 
 class itemVenda(models.Model):
         quantidade = models.IntegerField("Quantidade", null=true)
         itemVenda = models.ForeignKey(
-            "venda", on_delete=models.CASCADE, related_name"itemVenda"
-            "Produto", on_delete=models 
-        )
+            "venda", on_delete = models.CASCADE, related_name="itemVenda"
+            "Produto", on_delete=models.CASCADE, related_name="itemvendaProduto")
+    
+    def __str__(self):
+        return str(self.itemVenda)
         
     
 
